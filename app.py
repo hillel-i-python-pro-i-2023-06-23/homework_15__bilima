4874b4cc196e29589e5fe8c934e97c20994d62ab
nikusion <bilima.nika2003@gmail.com>
2023-08-17 11:19:12 +0300

    Create routes

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return "Hello world!"
+
+
+@app.route('/greet/<name>/<int:number>')
+def greet(name, number):
+    return f"Hello {name}! Your number is {number}."
+
+
+@app.route('/greet_query')
+def greet_query():
+    name = request.args.get('name')
+    number = request.args.get('number')
+    return f"Hello {name}! Your number is {number}."
+
+
+if __name__ == '__main__':
+    app.run()
